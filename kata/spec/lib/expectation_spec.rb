66fca528d10136151be8fd6b3ab3e0d3ef80d56f
hugolu <printk@gmail.com>
2014-12-04 17:29:03 +0800

    add examples

--- kata/spec/lib/expectation_spec.rb
+++ kata/spec/lib/expectation_spec.rb
@@ -1,3 +1,5 @@
+require_relative "../../lib/kata"
+
 describe "Expections" do
 	
 	context "Equivalence" do
@@ -46,4 +48,94 @@ describe "Expections" do
 		end
 	end
 
-end
+	context "Comparisons" do
+		specify "expect(actual).to be >  expected" do
+			expect(3).to be > 2
+		end
+
+		specify "expect(actual).to be >=  expected" do
+			expect(3).to be >= 3
+		end
+
+		specify "expect(actual).to be <=  expected" do
+			expect(3).to be <= 3
+		end
+
+		specify "expect(actual).to be <  expected" do
+			expect(2).to be < 3
+		end
+
+		specify "expect(actual).to be_within(delta).of(expected)" do
+			expect(5).to be_within(2).of(7)	# 7-2 < 5 < 7+2
+		end
+	end
+
+	context "Regular expressions" do
+		specify "expect(actual).to match(/expression/)" do
+			expect("ABCDEFG").to match(/CDE/)
+		end
+
+		specify "expect(actual).not_to match(/expression/)" do
+			expect("ABCDEFG").not_to match(/CE/)
+		end
+	end
+
+	context "Types/classes" do
+		specify "expect(actual).to be_an_instance_of(expected)" do
+			# passes if actual.class == expected
+			expect(Kata.new).to be_an_instance_of(Kata)
+		end
+
+		specify "expect(actual).to be_a(expected)" do
+			# passes if actual.is_a?(expected)
+			class Kaka < Kata; end
+			expect(Kaka.new).to be_a(Kata)
+		end
+
+		specify "expect(actual).to be_an(expected)" do
+			# an alias for be_a
+			class Kaka < Kata; end
+			expect(Kaka.new).to be_an(Kata)
+		end
+
+		specify "expect(actual).to be_a_kind_of(expected)" do
+			# another alias
+			class Kaka < Kata; end
+			expect(Kaka.new).to be_a_kind_of(Kata)
+		end
+	end
+
+	context "Truthiness" do
+		specify "expect(actual).to be_truthy" do
+			# passes if actual is truthy (not nil or false)
+			expect(true).to be_truthy
+			expect(1).to be_truthy
+		end
+
+		specify "expect(actual).to be true" do
+			# passes if actual == true
+			expect(true).to be true
+		end
+
+		specify "expect(actual).to be_falsy" do
+			# passes if actual is falsy (nil or false)
+			expect(false).to be_falsy
+			expect(nil).to be_falsy
+		end
+
+		specify "expect(actual).to be false" do
+			# passes if actual == false
+			expect(false).to be false
+		end
+
+		specify "expect(actual).to be_nil" do
+			# passes if actual is nil
+			expect(nil).to be_nil
+		end
+
+		specify "expect(actual).to_not be_nil" do
+			# passes if actual is not nil
+			expect(1).to_not be_nil
+		end
+	end
+end 
